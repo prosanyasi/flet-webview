7645cbf89aa32923a56be49b7d371a2592fad76e
InesaFitsner <inesa@appveyor.com>
2025-01-09 09:31:01 -0800

    python files

---
+++ src/flet_web_view/__init__.py
@@ -0,0 +1,8 @@
+from flet_web_view.webview import (
+    WebView,
+    WebviewConsoleMessageEvent,
+    WebviewJavaScriptEvent,
+    WebviewLogLevelSeverity,
+    WebviewRequestMethod,
+    WebviewScrollEvent,
+)
